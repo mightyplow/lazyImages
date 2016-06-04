f0a8276943306c226474dca3e24e2e5fa75cab3d
mightyplow <mightyplow@gmail.com>
2016-06-05 00:10:21 +0200

    Create lazyImage.js
    first version of snippet source

---
+++ lazyImage.js
@@ -0,0 +1,131 @@
+(function () {
+    if (!(MutationObserver && IntersectionObserver && Set)) {
+        return;
+    }
+
+    var lazySrcAttribute = 'data-src';
+
+    var intersectionObserverOptions = {
+        threshold: 0
+    }
+
+    var mutationObserverOptions = {
+        childList: true,
+        subtree: true,
+        attributeFilter: ['src', 'srcset']
+    }
+
+    function onGetVisible (items) {
+        items.forEach(function (item) {
+            var target = item.target,
+                imageHelper = getImageHelper(target)
+
+            imageHelper.restoreSource(target)
+            intersectionObserver.unobserve(target)
+        })
+    }
+
+    function toArray (arraylike) {
+        return Array.prototype.slice.call(arraylike);
+    }
+
+    var lazyImage = (function () {
+        var sourceAttribute = 'src'
+
+        return {
+            storeSource: function (elem) {
+                elem.setAttribute(lazySrcAttribute, elem.getAttribute(sourceAttribute))
+                elem.removeAttribute(sourceAttribute)
+            },
+
+            restoreSource: function (elem) {
+                elem.setAttribute(sourceAttribute, elem.getAttribute(lazySrcAttribute))
+                elem.removeAttribute(lazySrcAttribute)
+            },
+
+            hasSource: function (elem) {
+                return !!elem.getAttribute(sourceAttribute)
+            }
+        }
+    }())
+
+    var lazyPicture = (function () {
+        var sourceAttribute = 'srcset'
+
+        return {
+            storeSource: function (elem) {
+                var sources = elem.querySelectorAll('source')
+
+                sources.forEach(function (source) {
+                    source.setAttribute(lazySrcAttribute, source.getAttribute(sourceAttribute))
+                    source.removeAttribute(sourceAttribute)
+                })
+            },
+
+            restoreSource: function (elem) {
+                var sources = elem.querySelectorAll('source')
+
+                sources.forEach(function (source) {
+                    source.setAttribute(sourceAttribute, source.getAttribute(lazySrcAttribute))
+                    source.removeAttribute(lazySrcAttribute)
+                })
+            },
+
+            hasSource: function (elem) {
+                var sources = toArray(elem.querySelectorAll('source'))
+                return sources.every(function (src) {
+                    return !!src.getAttribute(sourceAttribute)
+                })
+            }
+        }
+    }())
+
+    function getImageHelper (elem) {
+        if (elem instanceof HTMLImageElement) {
+            return lazyImage
+        } else if (elem instanceof HTMLPictureElement) {
+            return lazyPicture
+        }
+
+        return null;
+    }
+
+    function getMutationElements(mutations, selector) {
+        return mutations.reduce(function (acc, item) {
+            if (item.addedNodes) {
+                var nodes = Array.prototype.slice.call(item.addedNodes)
+
+                nodes.forEach(function (node) {
+                    if (node instanceof HTMLElement) {
+                        if (node.matches(selector)) {
+                            acc.add(node)
+                        }
+
+                        node.querySelectorAll(selector).forEach(function (node) {
+                            acc.add(node)
+                        });
+                    }
+                });
+            }
+
+            return acc
+        }, new Set())
+    }
+
+    var intersectionObserver = new IntersectionObserver(onGetVisible, intersectionObserverOptions)
+
+    var mutationObserver = new MutationObserver(function (mutations) {
+        var images = getMutationElements(mutations, 'img, picture')
+
+        images.forEach(function (image) {
+            var imageHelper = getImageHelper(image)
+
+            if (imageHelper.hasSource(image)) {
+                imageHelper.storeSource(image)
+                intersectionObserver.observe(image)
+            }
+        })
+    })
+
+    mutationObserver.observe(document.body, mutationObserverOptions)
+}())
