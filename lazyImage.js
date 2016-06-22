fba998fe47df64874a7665af51e83da8631f461d
Marcel Pflug <mapf@spreadshirt.net>
2016-06-22 19:19:50 +0200

    added comments to describe the code

--- lazyImage.js
+++ lazyImage.js
@@ -3,10 +3,12 @@
         IntersectionObserver = window['IntersectionObserver'],
         Set = window['Set']
 
+    // breaks the script if any of the requirements aren't fulfilled
     if (!(MutationObserver && IntersectionObserver && Set)) {
         return
     }
 
+    // prefix for the attribute, where the original source gets stored in
     var lazySrcPrefix = 'data-'
 
     var intersectionObserverOptions = {
@@ -18,18 +20,25 @@
         subtree: true
     }
 
+    // helper function to convert arraylike objects into arrays
     function toArray (arraylike) {
         return Array.prototype.slice.call(arraylike || [])
     }
 
+    // helper function to call the foreach method on a given array
+    // layed out into an extra function to reduce minimified size
     function forEach (iterable, fn) {
         iterable.forEach(fn)
     }
 
+    // finds childelements with a given selector
     function findChildren (selector) {
         return toArray(this.querySelectorAll(selector))
     }
 
+    // create a function, which moves an attribute to another attrubute
+    // sourceGetter is a function which returns elements, from which the sourceAttribute
+    // gets moved
     function createAttributeMoveFunction (sourceGetter, sourceAttribute, targetAttribute) {
         return forEach.bind(null, sourceGetter(), function (source) {
             var sourceValue = source.getAttribute(sourceAttribute)
@@ -41,10 +50,13 @@
         })
     }
 
+    // function which puts an element into an array
     function getImageSources () {
         return [this]
     }
 
+    // creates a helper object for image or picture elements
+    // with methods to store and restore the source attributes
     function getImageHelper (elem) {
         var sourceAttribute,
             backupAttribute,
@@ -68,6 +80,7 @@
         }
     }
 
+    // callback for intersection observer which restores the backuped source attribute
     function onGetVisible (items) {
         forEach(items, function (item) {
             var target = item.target,
@@ -78,6 +91,8 @@
         })
     }
 
+    // function for mutation observer which calls a callback on elements which
+    // match the given selector
     function onMutationElements (mutations, selector, elementCallback) {
         forEach(mutations, function (item) {
             forEach(item.addedNodes, function (node) {
@@ -94,6 +109,11 @@
 
     var intersectionObserver = new IntersectionObserver(onGetVisible, intersectionObserverOptions)
 
+    // creates an mutation observer which looks for img and picture elements
+    // and stores the sources of the images in an extra attribute to prevent immediate loading
+    // of the image
+    //
+    // the images get visible again, when the intersection observer recognizes them to be in the viewport
     var mutationObserver = new MutationObserver(function (mutations) {
         var processedItems = new Set()
 
